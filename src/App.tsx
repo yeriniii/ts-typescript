155cb62970e836c882bb4fd576bb2ff9bcea2f4c
yeriniii <yerini000303@naver.com>
2024-03-07 14:51:22 +0900

    add: vite+react+ts 초기설정 및 tailwind 초기설정

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-green-300 justify-center align-middle">
+        <h1 className="mt-12 font-bold text-5xl ">Typescipt Todolist</h1>
+
+        <button className="mt-12 transform-gpu rounded-full bg-gradient-to-r from-pink-500 to-purple-500 px-8 py-4 font-bold text-white transition-transform hover:-translate-y-1 hover:shadow-lg">
+          Click me!
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], //tailwind가 클래스생성시 고려해야하는 파일의 경로
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
